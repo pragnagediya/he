6ee30e306d5a0a4d1886c8393e4ffa74b4237a13
pragnagediaya <pragnagediyad@gmail.com>
2022-05-03 18:10:13 -0400

    javapractics

---
+++ src/learning01.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class learning01 {
+    public static void main(String []args){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("enter your number");
+        int a =scanner.nextInt();
+        System.out.println(a);
+       {for (int b=1; b<=10; b++){
+                System.out.println(a+ "*" +b+ "=" +a*b );}
+       }
+    }
+}
